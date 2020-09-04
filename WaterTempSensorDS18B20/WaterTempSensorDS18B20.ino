bb693d94e32c0a580903a1934420f790224105da
5cottyD <49736614+5cottyD@users.noreply.github.com>
2020-09-04 14:41:55 +1000

    Add files via upload

---
+++ WaterTempSensorDS18B20/WaterTempSensorDS18B20.ino
@@ -0,0 +1,29 @@
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
+#define ONE_WIRE_BUS 5
+
+OneWire oneWire(ONE_WIRE_BUS);
+
+DallasTemperature sensors(&oneWire);
+
+ float Celcius=0;
+ float Fahrenheit=0;
+void setup(void)
+{
+  
+  Serial.begin(9600);
+  sensors.begin();
+}
+
+void loop(void)
+{ 
+  sensors.requestTemperatures(); 
+  Celcius=sensors.getTempCByIndex(0);
+  Fahrenheit=sensors.toFahrenheit(Celcius);
+  Serial.print(" C  ");
+  Serial.print(Celcius);
+  Serial.print(" F  ");
+  Serial.println(Fahrenheit);
+  delay(1000);
+}
