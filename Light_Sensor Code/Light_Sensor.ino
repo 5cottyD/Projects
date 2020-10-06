f97c426c4140fe57d8de4d25c04c0fefc6b002c9
5cottyD <49736614+5cottyD@users.noreply.github.com>
2020-10-07 10:53:01 +1100

    Add files via upload

---
+++ Light_Sensor Code/Light_Sensor.ino
@@ -0,0 +1,26 @@
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  // reads the input on analog pin A0 (value between 0 and 1023)
+  int analogValue = analogRead(A0);
+
+  Serial.print("Analog reading = ");
+  Serial.print(analogValue);   // the raw analog reading
+
+  // We'll have a few threshholds, qualitatively determined
+  if (analogValue < 100) {
+    Serial.println(" - Very bright");
+  } else if (analogValue < 200) {
+    Serial.println(" - Bright");
+  } else if (analogValue < 500) {
+    Serial.println(" - Light");
+  } else if (analogValue < 800) {
+    Serial.println(" - Dim");
+  } else {
+    Serial.println(" - Dark");
+  }
+
+  delay(500);
+}
