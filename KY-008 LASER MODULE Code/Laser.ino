48a0aa9ecc8200a1fba7d09ad10c83ae1be7c828
5cottyD <49736614+5cottyD@users.noreply.github.com>
2021-03-09 11:41:51 +1100

    Add files via upload

---
+++ KY-008 LASER MODULE Code/Laser.ino
@@ -0,0 +1,13 @@
+int laserPin = 52;
+
+void setup ()
+{
+  pinMode(laserPin, OUTPUT);
+}
+
+void loop () {
+  digitalWrite(laserPin, HIGH);
+  delay(20);
+  digitalWrite(laserPin, LOW);
+  delay(20);
+}
