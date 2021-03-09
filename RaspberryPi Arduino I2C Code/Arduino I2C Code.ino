ddcf8c2c4c6d455e829f37d12820d60641afb9c7
5cottyD <49736614+5cottyD@users.noreply.github.com>
2021-03-09 14:37:18 +1100

    Add files via upload

---
+++ RaspberryPi Arduino I2C Code/Arduino I2C Code.ino
@@ -0,0 +1,13 @@
+int laserPin = 10;
+
+void setup ()
+{
+  pinMode(laserPin, OUTPUT);
+}
+
+void loop () {
+  digitalWrite(laserPin, HIGH);
+  delay(100);
+  digitalWrite(laserPin, LOW);
+  delay(100);
+}
