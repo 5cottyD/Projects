edf407f24a57bab5a599ab03710c3423e48bc0f6
5cottyD <49736614+5cottyD@users.noreply.github.com>
2021-12-08 17:00:22 +1100

    Add files via upload

---
+++ Arduino Rain Sensor Code/RainSensor.ino
@@ -0,0 +1,31 @@
+// Arduino master sketch
+
+#include <Wire.h>
+
+byte i2c_rcv;               // data received from I2C bus
+unsigned long time_start;   // start time in milliseconds
+//int stat_LED;               // status of LED: 1 = ON, 0 = OFF
+//int value_pot;             // potentiometer position
+
+void setup(){
+    Serial.begin(9600);
+  Wire.begin(); // join I2C bus as the master
+  
+  // initialize global variables
+  i2c_rcv = 255;
+  //time_start = millis();
+
+
+}
+
+void loop()
+{
+  Wire.requestFrom(0000000, 1);    // request potentiometer position from slave 0x08
+  if(Wire.available()) 
+  {        // read response from slave 0x08
+    i2c_rcv = Wire.read();
+   
+   // Serial.println(i2c_rcv); 
+  }
+  Serial.println(i2c_rcv); 
+}
